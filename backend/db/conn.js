ccf49ec833ae01ad3bc7986e8fb1caae3612d3f8
Tushar Dighe <dighetushar654@gmail.com>
2021-07-03 20:36:00 +0530

    update

---
+++ backend/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+Database = process.env.Mongo_Url
+
+// connecting with mongoDB 
+mongoose.connect(Database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then ( () => console.log("MongoDB Connected"))
+.catch ( (err) => console.log(err));
