891f66b66b4ef26257e7a2f82f110605ab4ef56c
Tushar Dighe <dighetushar654@gmail.com>
2021-07-03 20:36:17 +0530

    update

---
+++ backend/models/userSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    phone: {
+        type:String,
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    cpassword: {
+        type:String,
+        required:true
+    },
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
