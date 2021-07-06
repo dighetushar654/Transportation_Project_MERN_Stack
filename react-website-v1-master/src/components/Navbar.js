7c8a2bffc834663198b1b4af0a43e9015c787159
kiran364 <79898690+kiran364@users.noreply.github.com>
2021-07-06 18:49:42 +0530

    updated

--- react-website-v1-master/src/components/Navbar.js
+++ react-website-v1-master/src/components/Navbar.js
@@ -52,11 +52,11 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/products'
+                to='/registration'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                Products
+                Registration
               </Link>
             </li>
 
