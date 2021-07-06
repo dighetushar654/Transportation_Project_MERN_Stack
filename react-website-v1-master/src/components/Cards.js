bd744494a5c447d56c9889513054fa50eb263a25
kiran364 <79898690+kiran364@users.noreply.github.com>
2021-07-06 18:48:35 +0530

    updated

--- react-website-v1-master/src/components/Cards.js
+++ react-website-v1-master/src/components/Cards.js
@@ -10,35 +10,41 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/truck1.jpg'
+              src='images/hyd.jpg'
               text='Full Load Services'
-              label='TRUCKS'
+              label='HYDRAULIC TRUCKS'
               path='/services'
             />
             <CardItem
-              src='images/pick.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='PICK_UP'
+              src='images/ape.jpg'
+              text='Low load and 3 wheels'
+              label='APE MINI'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/minitrucks.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='MINI'
+              src='images/tempo.jpg'
+              text='Medium load'
+              label='TEMPO'
               path='/services'
             />
             <CardItem
-              src='images/private.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='PRIVATE'
+              src='images/tractor.jpg'
+              text='For all farming servicess'
+              label='TRACTOR'
               path='/products'
             />
             <CardItem
-              src='images/tractor.jpg'
-              text='The backbone of the farm'
-              label='TRACTORS'
+              src='images/mini.jpg'
+              text='Medium load carring capacity'
+              label='PICK UP'
+              path='/sign-up'
+            />
+              <CardItem
+              src='images/iceare.jpg'
+              text='Full load without hydraulic '
+              label='EICHER'
               path='/sign-up'
             />
           </ul>
