a3d95b60084b6cc08de6d64e6cf93f55432dcf7f
kiran364 <kumawatkiran364@gmail.com>
2021-07-08 17:21:59 +0530

    updated

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,10 +2,5 @@ const mongoose = require("mongoose");
 Database = process.env.Mongo_Url
 
 // connecting with mongoDB 
-mongoose.connect(Database, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-}).then ( () => console.log("MongoDB Connected"))
+mongoose.connect(Database, { useNewUrlParser: true, useUnifiedTopology: true }).then ( () => console.log("MongoDB Connected"))
 .catch ( (err) => console.log(err));
