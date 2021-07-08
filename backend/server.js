93abb443968a9f33603fe1224b7e5f5b53162d93
kiran364 <kumawatkiran364@gmail.com>
2021-07-08 17:22:37 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ const app = express();
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({path: './config.env'});
-require('./db/conn');
+dotenv.config();
+require("./db/conn");
 const userRoute = require("./routes/user");
+const vehicleRoute = require("./routes/vehicle");
 
 const router = express.Router();
 
 // const User = require("./models/userSchema");
 Port = process.env.Port;
-
+app.use(express.json());
 app.use("/registerUser", userRoute);
+app.use("/registerVehicle", vehicleRoute);
 
-app.listen(Port, () => {
-    console.log(`Server Running On Port ${Port}`);
+
+app.listen(4000, () => {
+    console.log(`Server Running On Port 4000`);
 })
