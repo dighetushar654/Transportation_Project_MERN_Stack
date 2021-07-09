b10fee66ff71af7387b67fe1eab2c5475518a37d
Tushar Dighe <dighetushar654@gmail.com>
2021-07-09 22:05:21 +0530

    updated

--- backend/models/homeimgModel.js
+++ backend/models/homeimgModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const homeimgSchema = new mongoose.Schema({
     img:{
-        type:String
+        type: String,
+        required:true 
     }
 },
 {timestamps: true});
