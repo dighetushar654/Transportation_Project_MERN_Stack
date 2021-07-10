a3af9a743725b1e25f282a5b412b57b788f0d2c4
Tushar Dighe <dighetushar654@gmail.com>
2021-07-10 16:53:23 +0530

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -12,10 +14,37 @@ const userSchema = new mongoose.Schema({
     },
     no: {
         type:Number,  
-    }
+    },
+    tokens:[
+        {
+            token:{
+                type:String
+            }
+        }
+    ]
 },
 {timestamps: true});
 
+
+//we are the hashing the password
+userSchema.pre('save', async function(next) {
+    if(this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+//we are genrating token
+userSchema.methods.genrateAuthToken = async function () {
+    try {
+        let token = jwt.sign({ _id:this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token:token });
+        await this.save();
+        return(token);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 const myuser = mongoose.model("myuser", userSchema);
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 router.post("/", async (req, res) => {
@@ -9,27 +11,68 @@ router.post("/", async (req, res) => {
       return res.status(422).json({error:"filled all the fields"});
     }
 
-    User.findOne({ email:email })
-        .then((userExist) => {
-          if(userExist) {
-            return res.status(422).json({error:"Email already exist"});
-          }
+    try {
+        const userExist = await User.findOne({ email:email })
+   
+        if(userExist) {
+          return res.status(422).json({error:"Email already exist"});
+        }
           
-          const newuser = new User({ name, password, email, no});
-          newuser.save().then(() => {
-            res.status(200).json({message: "User registerd succesfully..."});
-          }).catch((err) => res.status(500).json({err:"failed to registerd"}));
-    
-    }).catch(err => { console.log(err); })
+        const newuser = new User({ name, password, email, no});
+
+        const userRegister = await newuser.save();
+      
+        if(userRegister) {
+          res.status(200).json({message: "User registerd succesfully..."});
+        } else {
+          res.status(500).json({error: "Failed to register"})
+        }
+  
+    } catch(err) {
+      console.log(err);
+    }
   });
 
   router.get("/:id", async (req, res) => {
     try {
-      const data = await User.findById(req.params.id);
-      res.status(200).json(data);
+        const data = await User.findById(req.params.id);
+        res.status(200).json(data);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
   });
+
+router.post("/signin", async (req,res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+          res.status(400).json({error:"Plz fill the data"});
+        }
+
+        const userLogin = await User.findOne({email: email});
+        // console.log(userLogin);
+
+        if (userLogin) {
+
+          const isMatch = await bcrypt.compare(password, userLogin.password);
+          
+          const token = await userLogin.genrateAuthToken();
+          console.log(token);
+
+          if (!isMatch) {
+              res.status(400).json({error:"Invalid Password"});
+          } else {
+              res.status(200).json({message:"User Signin Successfully.."})
+          }
+        } else {
+              res.status(400).json({error:"Invalid Creditials"});
+        }
+        
+    } catch (err) {
+      res.status(500).json(err);
+      console.log(err);
+    }
+});
 
   module.exports = router; 
