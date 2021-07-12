74a255a00b1948d5becd32a5efaf5214a847acb6
kiran364 <kumawatkiran364@gmail.com>
2021-07-12 13:27:27 +0530

    updated

--- backend/models/vehicleModel.js
+++ backend/models/vehicleModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 const vehicleSchema = new mongoose.Schema({
     name: {
@@ -32,6 +34,18 @@ vehicleSchema.pre('save', async function(next) {
     }
     next();
 });
+
+//we are genrating token
+vehicleSchema.methods.genrateAuthToken = async function () {
+    try {
+        let token = jwt.sign({ _id:this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token:token });
+        await this.save();
+        return(token);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 
 module.exports = mongoose.model("myVehicle", vehicleSchema);
