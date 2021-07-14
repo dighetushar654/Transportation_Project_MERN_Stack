4c0111166507e7aaa283bee53faf100957cf4d8c
Tushar Dighe <dighetushar654@gmail.com>
2021-07-14 21:04:06 +0530

    updated

--- react-website-v1-master/src/App.js
+++ react-website-v1-master/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Navbar from './components/Navbar';
 import './App.css';
+import { Provider } from 'react-redux';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Registration from './components/pages/Registration';
 import SignUp from './components/pages/SignUp';
 import VehicleRegistration from "./components/pages/VehicleRegistration";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducers } from './reducers';
+import './index.css';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 
 class App extends Component {
 render() {
@@ -16,11 +25,14 @@ render() {
       <Router>
         <Navbar />
         <Switch>
+        <Provider store={store}>
+
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/vehicle' component={VehicleRegistration} />
           <Route path='/registration' component={Registration} />
           <Route path='/sign-up' component={SignUp} />
+        </Provider>
 
         </Switch>
       </Router>
--- react-website-v1-master/src/components/pages/Services.js
+++ react-website-v1-master/src/components/pages/Services.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import axios from "axios";
-import '../../App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-export default class vehicleList extends React.Component {
-  state = {
-    vehicle: [],
-  };
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import { getPosts } from '../../actions/posts';
+import useStyles from '../../styles';
+// import memories from '../../images/memories.png';
 
-  componentDidMount() {
-    axios.get("http://localhost:4000/homeimages").then(res => {
-      console.log(res);
-      this.setState({vehicle: res.data});
-    })
-  }
-}
+const Services = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Vehicle Owner</Typography>
+        {/* <img className={classes.image} src={memories} alt="icon" height="60" /> */}
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default Services;
