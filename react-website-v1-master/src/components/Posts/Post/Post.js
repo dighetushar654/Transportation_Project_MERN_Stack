232111538ceb38d9fc5fd7f8181bf6719af6e045
Tushar Dighe <dighetushar654@gmail.com>
2021-07-14 22:26:38 +0530

    updated

--- react-website-v1-master/src/components/Posts/Post/Post.js
+++ react-website-v1-master/src/components/Posts/Post/Post.js
@@ -5,6 +5,8 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
+import Switch from '@material-ui/core/Switch';
+
 
 import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
@@ -31,7 +33,7 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        {/* <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button> */}
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><Switch fontSize="small" /> Status {post.likeCount} </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
       </CardActions>
     </Card>
