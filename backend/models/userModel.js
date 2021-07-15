18c37b3e77e6f23b3b3b6f43fc8ca5e3c76b2c80
kiran364 <kumawatkiran364@gmail.com>
2021-07-15 17:26:37 +0530

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,6 +36,7 @@ userSchema.pre('save', async function(next) {
 
 //we are genrating token
 userSchema.methods.genrateAuthToken = async function () {
+    
     try {
         let token = jwt.sign({ _id:this._id }, process.env.SECRET_KEY);
         this.tokens = this.tokens.concat({ token:token });
