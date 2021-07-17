70694b2237a5f9c59847461e05037be608a585c8
kiran364 <kumawatkiran364@gmail.com>
2021-07-17 14:15:33 +0530

    updated

--- react-website-v1-master/src/App.js
+++ react-website-v1-master/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Navbar from './components/Navbar';
 import './App.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import { Provider } from 'react-redux';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Registration from './components/pages/Registration';
 import SignUp from './components/pages/SignUp';
+import UserHome from "./components/pages/UserHome";
+
 import VehicleRegistration from "./components/pages/VehicleRegistration";
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -14,6 +17,10 @@ import thunk from 'redux-thunk';
 import { reducers } from './reducers';
 import './index.css';
 
+
+
+import AddUser from "./components/users/AddUser";
+import EditUser from "./components/users/EditUser";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 
@@ -29,6 +36,9 @@ render() {
 
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
+          <Route path="/UserHome" component={UserHome} />
+          <Route path="/users/add" exact component={AddUser} />
+          <Route path="/users/edit/:id" component={EditUser} />
           <Route path='/vehicle' component={VehicleRegistration} />
           <Route path='/registration' component={Registration} />
           <Route path='/sign-up' component={SignUp} />
