5f4b3ac815721d31bfcfef2728c7b637ea127a74
Tushar Dighe <dighetushar654@gmail.com>
2021-07-22 22:19:02 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ const postRoutes = require("./routes/posts.js")
 
 const router = express.Router();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors());
 
 app.use(express.json());
